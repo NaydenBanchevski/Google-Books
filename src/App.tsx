98bf4aadf6f5262c5b2bacbb4363fc85c71a5a6a
Nayden Banchevski <163778420+NaydenBanchevski@users.noreply.github.com>
2024-10-05 12:07:18 -0500

    First Commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import { SparklesPreview } from "./components/Sparkles";
+
+function App() {
+  return (
+    <>
+      <SparklesPreview />
+    </>
+  );
+}
+
+export default App;
